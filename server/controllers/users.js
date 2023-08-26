29ab282d6a5ce6a26f132ba481ffa826c73e4e89
Henry <sssh10130338@gmail.com>
2023-08-26 19:39:22 +0800

    study the server part

---
+++ server/controllers/users.js
@@ -0,0 +1,67 @@
+import User from "../models/User.js";
+
+/* READ */
+// (getUser) 取得使用者資訊
+export const getUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+// (getUserFriends) 取得使用者的朋友資訊
+export const getUserFriends = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
+    res.status(200).json(formattedFriends);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+/* UPDATE */
+export const addRemoveFriend = async (req, res) => {
+  try {
+    const { id, friendId } = req.params;
+    const user = await User.findById(id);
+    const friend = await User.findById(friendId);
+
+    if (user.friends.includes(friendId)) {
+      // 如果兩個人有朋友關係，則將彼此從自己的朋友名單中去除。
+      user.friends = user.friends.filter((id) => id !== friendId);
+      friend.friends = friend.friends.filter((id) => id !== id);
+    } else {
+      // 否則將彼此加入自己的朋友名單。
+      user.friends.push(friendId);
+      friend.friends.push(id);
+    }
+    await user.save();
+    await friend.save();
+
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
+
+    res.status(200).json(formattedFriends);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -1,24 +1,30 @@
 import express from "express";
+
 import bodyParser from "body-parser";
+// body-parser(https://www.npmjs.com/package/body-parser)
+
 import mongoose from "mongoose";
 import cors from "cors";
+// cors(https://www.npmjs.com/package/cors)
+// Learn CORS In 6 Minutes(https://www.youtube.com/watch?v=PNtFSVU-YTI)
+
 import dotenv from "dotenv";
 import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-/*
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+
+/* TEST DATA */
 import User from "./models/User.js";
 import Post from "./models/Post.js";
 import { users, posts } from "./data/index.js";
-*/
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -45,16 +51,25 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
+/* ROUTES 的部分，
+   會分為 (ROUTES WITH FILES) 和 (ROUTES)，
+   是因為 (ROUTES WITH FILES) 寫在這可以直接使用到 (upload)，
+   而 (ROUTES) 沒有需要使用到，就寫到 (/routes) 中管理。 */
+
 /* ROUTES WITH FILES */
-// app.post("/auth/register", upload.single("picture"), register);
-// app.post("/posts", verifyToken, upload.single("picture"), createPost);
+app.post("/auth/register", upload.single("picture"), register);
+/* 在 post (/auth/register) 時，
+   會呼叫 (upload.single("picture")) 這個 Middleware，
+   也會呼叫 (/controllers/auth.js) 中的 register。 */
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
-// app.use("/auth", authRoutes);
-// app.use("/users", userRoutes);
-// app.use("/posts", postRoutes);
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
+// 連接 MongoDB
 const PORT = process.env.PORT || 6001;
 mongoose
   .connect(process.env.MONGO_URL, {
