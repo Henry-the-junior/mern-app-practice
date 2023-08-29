11a9c44486a0d2a3e726596f951f552fc182aa05
Henry <sssh10130338@gmail.com>
2023-08-29 23:19:36 +0800

    start to study the login page

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -39,10 +39,10 @@ const Navbar = () => {
   const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
 
-  // const fullName = `${user.firstName} ${user.lastName}`;
+  const fullName = `${user.firstName} ${user.lastName}`;
   
   // test fullName
-  const fullName = `Henry Tsai`;
+  // const fullName = `Henry Tsai`;
 
   return (
     <FlexBetween padding="1rem 6%" backgroundColor={alt}>
