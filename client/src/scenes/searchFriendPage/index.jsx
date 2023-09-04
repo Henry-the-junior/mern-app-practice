074bc3cb18e082edf5a513e1b1da5211d80cb2de
Henry <sssh10130338@gmail.com>
2023-09-04 14:24:24 +0800

    complete the search users feature

--- client/src/scenes/searchFriendPage/index.jsx
+++ client/src/scenes/searchFriendPage/index.jsx
@@ -1,9 +1,9 @@
-import { useState, useEffect } from "react";
 import { Box, useMediaQuery } from "@mui/material";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import Navbar from "scenes/navbar";
 import UserWidget from "scenes/widgets/UserWidget";
+import UsersWidget from "scenes/widgets/UsersWidget";
 import AdvertWidget from "scenes/widgets/AdvertWidget";
 import FriendListWidget from "scenes/widgets/FriendListWidget";
 
@@ -12,28 +12,6 @@ const SearchFriendPage = () => {
   const { _id, picturePath } = useSelector((state) => state.user);
 
   const { searchName } = useParams();
-  const dispatch = useDispatch();
-  const token = useSelector((state) => state.token);
-  const [users, setUsers] = useState([]);
-
-  const searchUsers = async () => {
-    const response = await fetch(
-      `http://localhost:3001/users/searchfriends/${searchName}`,
-      {
-        method: "GET",
-        headers: { Authorization: `Bearer ${token}` },
-      }
-    );
-    const data = await response.json();
-    setUsers({ data });
-  };
-
-  useEffect(() => {
-    searchUsers();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
-  console.log("users");
-  console.log(users);
 
   return (
     <Box>
@@ -52,6 +30,7 @@ const SearchFriendPage = () => {
           flexBasis={isNonMobileScreens ? "42%" : undefined}
           mt={isNonMobileScreens ? undefined : "2rem"}
         >
+          <UsersWidget searchName={searchName} />
         </Box>
         {isNonMobileScreens && (
           <Box flexBasis="26%">
---
+++ client/src/scenes/widgets/UsersWidget.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import Friend from "components/Friend";
+import WidgetWrapper from "components/WidgetWrapper";
+
+const UsersWidget = ({ searchName }) => {
+  const token = useSelector((state) => state.token);
+  const [users, setUsers] = useState([]);
+
+  const searchUsers = async () => {
+    const response = await fetch(
+      `http://localhost:3001/users/searchfriends/${searchName}`,
+      {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    const data = await response.json();
+    setUsers(data);
+  };
+
+  useEffect(() => {
+    searchUsers();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <>
+      {users.map(
+        ({
+          _id,
+          firstName,
+          lastName,
+          occupation,
+          location,
+          picturePath,
+        }) => (
+          <WidgetWrapper m="2rem 0">
+            <Friend
+              friendId={_id}
+              subtitle={occupation}
+              name={`${firstName} ${lastName}`}
+              userPicturePath={picturePath}
+            />
+          </WidgetWrapper>
+        )
+      )}
+    </>
+  );
+};
+
+export default UsersWidget;
